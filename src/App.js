de586d9161fb5431d87d75bfb47a5c2231ad659e
case2500 <case2500@hotmail.com>
2023-12-21 08:54:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { useState } from "react";
+
+
+import { Table } from "./components/Table";
+import { Modal } from "./components/Modal";
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      page: "Home",
+      description: "This is the main page of the website",
+      status: "live",
+    },
+    {
+      page: "About Us",
+      description: "This page has details about the company",
+      status: "draft",
+    },
+    {
+      page: "Pricing",
+      description: "Prices for different subscriptions",
+      status: "error",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+alert(idx)
+
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
+
+  return (
+    <div className="App">
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn">
+        Add
+      </button>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+          //rows[0] => export const Modal = ({ closeModal, onSubmit, defaultValue }) => {
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
